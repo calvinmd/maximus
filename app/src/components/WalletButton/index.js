89a69a4635ab3a50cd10f6a757447657e0cce44c
Calvin Pak <calvinpak@gmail.com>
2020-05-17 15:21:04 -0700

    added auto-refresh for address change. got a bug after network change it redirects to home page, weird.

--- app/src/components/WalletButton/index.js
+++ app/src/components/WalletButton/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { get } from "lodash";
 import { Button } from '../Button';
 
+// Map supports integer keys and faster in general
 const networkName = new Map();
 networkName.set(1, 'Main Net');
 networkName.set(3, 'Ropsten');
@@ -11,31 +12,46 @@ networkName.set(42, 'Kovan');
 networkName.set(8545, 'Localhost');
 
 const WalletButton = () => {
+  const [addressChangeCount, setAddressChangeCount] = useState(0);
+  const [networkChangeCount, setNetworkChangeCount] = useState(0);
+  console.log('Address changed ', addressChangeCount, ' times.');
+  console.log('Network changed ', networkChangeCount, ' times.');
+  const hasEthereum = typeof window !== 'undefined' && typeof window.ethereum !== 'undefined';
+  const selectedAddress = get(window, 'ethereum.selectedAddress');
+  const networkId = Number(get(window, 'ethereum.networkVersion'));
 
   let onClick = () => { };
-  let buttonLabel = "Connect Wallet";
+  let buttonLabel = "Checking connection...";
 
-  if (typeof window.ethereum === 'undefined') {
+  if (!hasEthereum) {
     console.log('No metamask installed');
     buttonLabel = "Install Metamask";
     onClick = () => window.open("https://metamask.io/");
-  } else if (
-    window &&
-    window.ethereum &&
-    window.ethereum.selectedAddress
-  ) {
+  } else if (selectedAddress && networkId) {
     console.log('Wallet connected');
-    const address = window.ethereum.selectedAddress;
-    const networkId = Number(window.ethereum.networkVersion);
-    buttonLabel = `${address} (${networkName.get(networkId)})`;
+    buttonLabel = `${selectedAddress} (${networkName.get(networkId)})`;
   } else {
+    buttonLabel = "Connect Wallet";
     onClick = () => {
       console.log("Enable wallet");
       window.ethereum.enable();
     };
   }
-  console.log('zzz buttonLabel: ', buttonLabel);
-  console.log('zzz onClick: ', onClick);
+
+  useEffect(() => {
+    window.ethereum.on('accountsChanged', (account) => {
+      console.log('accountsChanged: ', account);
+      setAddressChangeCount(addressChangeCount + 1);
+    });
+  }, [typeof window !== 'undefined' && typeof window.ethereum !== 'undefined' && window.ethereum.selectedAddress]);
+
+  useEffect(() => {
+    window.ethereum.on('networkChanged', (network) => {
+      console.log('networkChanged ', network);
+      setNetworkChangeCount(networkChangeCount + 1);
+    });
+  }, [typeof window !== 'undefined' && typeof window.ethereum !== 'undefined' && window.ethereum.networkVersion]);
+
   return (
     <Button
       type="button"
