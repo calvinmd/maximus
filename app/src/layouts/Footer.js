e7f5b2837e07967c6ad14cf8e4689aa802329ef4
Shain <srslafazan@gmail.com>
2020-05-17 20:47:31 -0700

    Updated footer

--- app/src/layouts/Footer.js
+++ app/src/layouts/Footer.js
@@ -1,21 +1,21 @@
 import React from "react";
 
 const Footer = () => (
-  <div class="flex justify-between">
+  <div className="flex justify-between">
     <div>
       Maximus &copy; 2020.
     </div>
-    <div class="text-sm">
+    <div className="text-sm">
       Contributors:&nbsp;&nbsp;
-        <a href="https://twitter.com/andrew_bakst" target="_blank">Andrew</a>,&nbsp;
-        <a href="https://github.com/srslafazan" target="_blank">Shain</a>,&nbsp;
-        <a href="https://github.com/calvinmd" target="_blank">Calvin</a>,&nbsp;
-        <a href="https://github.com/garythung" target="_blank">Gary</a>.
+        <a href="https://twitter.com/andrew_bakst" target="_blank" rel="noopener noreferrer">Andrew</a>,&nbsp;
+        <a href="https://github.com/srslafazan" target="_blank" rel="noopener noreferrer">Shain</a>,&nbsp;
+        <a href="https://github.com/calvinmd" target="_blank" rel="noopener noreferrer">Calvin</a>,&nbsp;
+        <a href="https://github.com/garythung" target="_blank" rel="noopener noreferrer">Gary</a>.
     </div>
     <div>
       <div>
-        <a href="https://twitter.com/maximus__bank" target="_blank">
-          <img src="/twitter_circle.png" height="24px" width="24px" />
+        <a href="https://twitter.com/maximus__bank" target="_blank" rel="noopener noreferrer">
+          <img alt="twitter" src="/twitter_circle.png" height="24px" width="24px" />
         </a>
       </div>
     </div>
