f561e3370fe452f00566d48bd46ac20d9b900fe8
Shain <srslafazan@gmail.com>
2020-05-23 11:21:49 -0700

    Updated playground

--- app/src/pages/Playground.js
+++ app/src/pages/Playground.js
@@ -7,18 +7,37 @@ import {
   borrowUSDM,
 } from '../util'
 
+const style = { border: '1px solid black', padding: '10px 20px' }
 
 const Playground = () => {
+  const [lendEthAmount, setLendEthAmount] = React.useState(1)
+  const [lendUSDMAmount, setLendUSDMAmount] = React.useState(1)
+  const [borrowUSDMAmount, setBorrowUSDMAmount] = React.useState(1)
   return (
     <div>
-      <h1>Playground</h1>
-
-      <button style={{ border: '1px solid black', padding: '20px' }} onClick={() => enterMarketUSDM()}>enterMarketUSDM</button>
-      <button style={{ border: '1px solid black', padding: '20px' }} onClick={() => lendETH(1)}>lendETH</button>
-      <button style={{ border: '1px solid black', padding: '20px' }} onClick={() => lendUSDM(1)}>lendUSDM</button>
-      <button style={{ border: '1px solid black', padding: '20px' }} onClick={() => borrowUSDM(1)}>borrowUSDM</button>
+      <div>
+        <div style={{ padding: '20px', display: 'flex', justifyContent: 'space-between' }}>
+          <label>Authorize the money market application.</label>
+          <button style={style} onClick={() => enterMarketUSDM()}>enterMarketUSDM</button>
+        </div>
+        <div style={{ padding: '20px', display: 'flex', justifyContent: 'space-between' }}>
+          <label>Lend Eth for interest, and/or lock for collateral to borrow USDM later.</label>
+          <input className="border-b-2 border-black text-5xl" placeholder="0.000" type="number" value={lendEthAmount} onChange={(val) => setLendEthAmount(val)} />
+          <button style={style} onClick={() => lendETH(lendEthAmount)}>lendETH</button>
+        </div>
+        <div style={{ padding: '20px', display: 'flex', justifyContent: 'space-between' }}>
+          <label>Lend USDM</label>
+          <input className="border-b-2 border-black text-5xl" placeholder="0.000" type="number" value={lendUSDMAmount} onChange={(val) => setLendUSDMAmount(val)} />
+          <button style={style} onClick={() => lendUSDM(lendUSDMAmount)}>lendUSDM</button>
+        </div>
+        <div style={{ padding: '20px', display: 'flex', justifyContent: 'space-between' }}>
+          <label>Lend USDM</label>
+          <input className="border-b-2 border-black text-5xl" placeholder="0.000" type="number" value={borrowUSDMAmount} onChange={(val) => setBorrowUSDMAmount(val)} />
+          <button style={style} onClick={() => borrowUSDM(borrowUSDMAmount)}>borrowUSDM</button>
+        </div>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export { Playground };
+export { Playground }
